6c9826fc0775a4e8dbbf4ee809607bed3fd9bf58
skoulouzis <S.Koulouzis@uva.nl>
2016-12-02 15:08:37 +0100

    debug messages

--- src/main/java/nl/uva/cpp/WordCount.java
+++ src/main/java/nl/uva/cpp/WordCount.java
@@ -14,17 +14,25 @@ public class WordCount {
 
 //      example1(args);
 //      runJar(args);
+      long startRun1 = System.currentTimeMillis();
       runJob(args);
+      long elapsedRun1 = System.currentTimeMillis() - startRun1;
 
+      long startRun2 = System.currentTimeMillis();
+      runJobWithToolRunner(args);
+      long elapsedRun2 = System.currentTimeMillis() - startRun2;
+
+      System.err.println("runJob elapsed: " + elapsedRun1 + "ms");
+      System.err.println("runJobWithToolRunner elapsed: " + elapsedRun2 + "ms");
     } catch (Throwable ex) {
       Logger.getLogger(WordCount.class.getName()).log(Level.SEVERE, null, ex);
     }
 
   }
 
-  private static void example1(String[] args) throws Exception {
+  private static void runJobWithToolRunner(String[] args) throws Exception {
 
-//      args = new String[]{System.getProperty("user.home") + "/Downloads/trainData", System.getProperty("user.home") + "/Downloads/out"};
+    args = new String[]{System.getProperty("user.home") + "/Downloads/trainData", System.getProperty("user.home") + "/Downloads/out"};
     int retval = 0;
 
     retval = ToolRunner.run(new Configuration(), new WordCountTool(), args);
