cd3d7e73faa0aea9537b3922c01dce39450a7964
skoulouzis <S.Koulouzis@uva.nl>
2016-12-20 15:53:55 +0100

    added etc folder

--- src/main/java/nl/uva/cpp/WordCountTool.java
+++ src/main/java/nl/uva/cpp/WordCountTool.java
@@ -1,6 +1,8 @@
 
 package nl.uva.cpp;
 
+import java.io.File;
+import java.io.FilenameFilter;
 import org.apache.hadoop.conf.*;
 import org.apache.hadoop.fs.*;
 import org.apache.hadoop.io.*;
@@ -14,11 +16,12 @@ public class WordCountTool extends Configured implements Tool {
   @Override
   public int run(String[] args) throws Exception {
     Configuration conf = this.getConf();
-    if (!args[2].equals("local")) {
-      conf.set(FileSystem.FS_DEFAULT_NAME_KEY, args[3]);
-      conf.set("mapreduce.framework.name", "yarn");
-      conf.set("yarn.resourcemanager.address", args[4]);
-      conf.set("fs.hdfs.impl", org.apache.hadoop.hdfs.DistributedFileSystem.class.getName());
+    if (args.length > 2 && args[2] != null) {
+      conf = addPropertiesToConf(conf, args[2]);
+//      conf.set(FileSystem.FS_DEFAULT_NAME_KEY, args[3]);
+//      conf.set("mapreduce.framework.name", "yarn");
+//      conf.set("yarn.resourcemanager.address", args[4]);
+//      conf.set("fs.hdfs.impl", org.apache.hadoop.hdfs.DistributedFileSystem.class.getName());
     }
 
     Job job = Job.getInstance(conf);
@@ -50,5 +53,26 @@ public class WordCountTool extends Configured implements Tool {
     // job.getConfiguration().setInt("mapreduce.job.running.map.limit", numTasks);
     // Run the job!
     return job.waitForCompletion(true) ? 0 : 1;
+  }
+
+  private Configuration addPropertiesToConf(Configuration conf, String etcPath) {
+    File etc = new File(etcPath);
+    File[] files = etc.listFiles(new FilenameFilter() {
+      @Override
+      public boolean accept(File dir, String name) {
+        return name.toLowerCase().endsWith(".xml");
+      }
+    });
+    if (files != null) {
+      for (File f : files) {
+        conf.addResource(new org.apache.hadoop.fs.Path(f.getAbsolutePath()));
+      }
+      conf.set("fs.hdfs.impl", org.apache.hadoop.hdfs.DistributedFileSystem.class.getName());
+    } else {
+//      conf.set("mapreduce.framework.name", "local");
+//      conf.set("mapred.job.tracker", "local");
+//      conf.set("fs.defaultFS", "file:///");
+    }
+    return conf;
   }
 }
